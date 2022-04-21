4642c63195c33064219c6901295315851ee3a22c
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-04-21 09:31:50 +0530

    feat/adding overlay with hamburger menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,15 @@
 console.log('test');
 let hamburger_menu = document.querySelector('.hamburger-menu');
+let overlay = document.querySelector('.overlay-mobile-menu');
 
 hamburger_menu.addEventListener('click', function(){
   if (hamburger_menu.classList.contains('open')) {
     hamburger_menu.classList.remove('open');
+    overlay.classList.remove('fade-in');
+    overlay.classList.add('fade-out');
   } else {
     hamburger_menu.classList.add('open');
+    overlay.classList.add('fade-in');
+    overlay.classList.remove('fade-out');
   }
 });
