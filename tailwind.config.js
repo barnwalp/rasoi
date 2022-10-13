6b540f50d087dfe34360373aea6e8fe2ebd8e690
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-10-13 20:50:51 +0530

    feat/changed typo in app and added custom colors in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ module.exports = {
 		"./src/**/*.{js,jsx,ts,tsx}",
 	],
   theme: {
+		colors: {
+			primary: '#E67E22',
+			darkprimary: '#CF6D17',
+			lightgray: '#F4F4F4',
+			darkgray: '#2D314D',
+		},
 		extend: {
 			backgroundImage: {
 				'hero-pattern': "linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url('/src/assets/hero_picture.jpg')",
